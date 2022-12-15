080ce27ce9107e425cc6ba3ed89f5ad4f120b8e2
taeyun-work <wmfajr1357@gmail.com>
2022-12-15 19:34:49 +0900

    feat: 유저의 입력이 잘못되었을 때 예외 발생시키는 기능 구현
    세자리의 숫자가 아닌 다른 문자가 오면 예외 처리,
    세자리 수 중 중복된 수가 존재하면 예외 처리

--- src/main/java/baseball/domain/UserBall.java
+++ src/main/java/baseball/domain/UserBall.java
@@ -2,17 +2,34 @@ package baseball.domain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class UserBall {
     List<Integer> computerBall;
 
     public UserBall(String inputBall) {
+        checkBallFormat(inputBall);
+
         this.computerBall = new ArrayList<Integer>();
         String [] balls = inputBall.split("");
         Arrays.stream(balls)
                 .forEach(ball -> {
                     computerBall.add(Integer.parseInt(ball));
                 });
+    }
+
+    private void checkBallFormat(String inputBall) {
+        String ballFormatRegex = "^[1-9]{3}$";
+        if(!inputBall.matches(ballFormatRegex)) {
+            throw new IllegalArgumentException("[ERROR] 올바른 수를 입력해 주세요.");
+        }
+    }
+
+    private void checkBallDuplication(String[] balls) {
+        HashSet<String> validBalls = new HashSet<String>(List.of(balls));
+        if(validBalls.size() != 3) {
+            throw new IllegalArgumentException("[ERROR] 중복이 되지 않도록 입력해 주세요.");
+        }
     }
 }
